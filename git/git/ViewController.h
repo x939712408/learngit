5476e224c6285f52d21be7692d29fc1e99a8e6ed
kiss-minmin <kiss-minmin@kiss-minmindemacbook-pro.local>
2016-09-01 10:26:18 +0800

    开始第一个文件

---
+++ git/git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git
+//
+//  Created by kiss-minmin on 16/9/1.
+//  Copyright © 2016年 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ git/git/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  git
+//
+//  Created by kiss-minmin on 16/9/1.
+//  Copyright © 2016年 Big Nerd Ranch. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"哈哈");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
